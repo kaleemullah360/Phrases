b9417d10967dbffddb998e5d6bddc9b8e607b03d
Kaleem Ullah <kaleemullah360@live.com>
2018-05-20 00:13:50 +0500

    set-host-port-baspath

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+	root "main#list"
   match "main/list", to: "main#list", as: "main_list", via: [:get, :post]
   match "main/count/phrases", to: "main#count_phrases", as: "count_phrases", via: [:get, :post]
   match "main/get/phrases", to: "main#get_phrases", as: "get_phrases", via: [:get, :post]
