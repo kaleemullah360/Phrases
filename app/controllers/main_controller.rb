b8c75f98482c21b7f21fd4ef33dae8c95e3cbf7b
Kaleem Ullah <kaleemullah360@live.com>
2018-05-20 05:13:14 +0500

    improved-phrases-state-on-reload

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,10 @@
 class MainController < ApplicationController
   def list
-  	@phrases = Main.all
+    @phrases = Main.all
   end
 
   def count_phrases
-  	
+
     response = {}
     response[:phrases] = []
     response[:status] = '0'
@@ -22,13 +22,12 @@ class MainController < ApplicationController
     rescue => e
       response[:message] = e.message
     end
-     render json: response, :except=>  [:created_at, :updated_at] and return
+    render json: response, :except=>  [:created_at, :updated_at] and return
 
-end
-
+  end
 
   def get_phrases
-  	
+
     response = {}
     response[:phrases] = []
     response[:status] = '0'
@@ -37,6 +36,7 @@ end
       phrases = Main.find_by(id: params[:id] || 1)
 
       if phrases
+        set_history params[:id]
         response[:message] = "phrases [ #{phrases.id} ] found."
         response[:phrases] = phrases
       else
@@ -46,8 +46,68 @@ end
     rescue => e
       response[:message] = e.message
     end
-     render json: response, :except=>  [:created_at, :updated_at] and return
+    render json: response, :except=>  [:created_at, :updated_at] and return
 
-end
+  end
+
+  def phrases_history
+    response = {}
+    response[:phrases] = []
+    response[:status] = '0'
+
+    begin
+      phrases = get_history
+
+      if phrases
+        response[:message] = "phrases [ #{phrases.count} ] found."
+        response[:phrases] = phrases
+      else
+        response[:message] = 'Unable to find phrases'
+      end
+
+    rescue => e
+      response[:message] = e.message
+    end
+    render json: response, :except=>  [:created_at, :updated_at] and return
+
+  end
+
+  def clear_history
+    response = {}
+    response[:phrases] = []
+    response[:status] = '0'
+
+    begin
+      clear_session
+
+      phrases = Main.all.count
+      if phrases
+        response[:message] = "phrases [ #{phrases} ] found."
+        response[:phrases] = phrases
+      else
+        response[:message] = 'Unable to find phrases'
+      end
+
+    rescue => e
+      response[:message] = e.message
+    end
+    render json: response, :except=>  [:created_at, :updated_at] and return
+
+  end
+
+  def set_history id
+    session[:phrases] ||= []
+    if session[:phrases].exclude?(id)
+      session[:phrases].push(id)
+    end
+  end
+
+  def clear_session
+    session.delete(:phrases)
+  end
+
+  def get_history
+    return session[:phrases]
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   match "main/list", to: "main#list", as: "main_list", via: [:get, :post]
   match "main/count/phrases", to: "main#count_phrases", as: "count_phrases", via: [:get, :post]
   match "main/get/phrases", to: "main#get_phrases", as: "get_phrases", via: [:get, :post]
+  match "main/get/history", to: "main#phrases_history", as: "phrases_history", via: [:get, :post]
+  match "main/clear/history", to: "main#clear_history", as: "clear_history", via: [:get, :post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
